d8216aa45a1e3c0146baaacd1a34a00e554487fb
Mustafa ASLAN <mmustafaslann1453@gmail.com>
2022-11-10 14:11:38 +0300

    10.11.2022

---
+++ src/geometri/Daire.java
@@ -0,0 +1,16 @@
+package geometri;
+
+public class Daire {
+
+    public static double alanHesapla(int yaricap){
+        return (double) (Math.PI * yaricap * yaricap);
+
+
+    }
+    public  static  double cevreHesapla(int yaricap){
+        return (double) (2 * Math.PI * yaricap);
+    }
+
+
+    }
+
--- src/geometri/MainApp.java
+++ src/geometri/MainApp.java
@@ -4,5 +4,28 @@ public class MainApp {
     public static void main(String[] args) {
         System.out.println();
         //Lütfen readme.md dosyasına göre ödevi tamamlayınız.
+
+        int alanHesapla = Kare.alanHesapla(7 );
+        System.out.println("Karenin Alanı :"+alanHesapla);
+
+        int cevreHesapla = Kare.cevreHesapla(7);
+        System.out.println("Karenin Çevresi :"+cevreHesapla);
+
+        double kosegenUzunlugu = Kare.kosegenUzunlugu(7);
+        System.out.println("köşegen Uzunluğu :"+kosegenUzunlugu);
+
+
+        double alanHesapla1 = Daire.alanHesapla(7);
+        System.out.println("Dairenin Alanı :"+alanHesapla1);
+
+        double cevreHesapla1 = Daire.cevreHesapla(7);
+        System.out.println("Dairenin Çevresi :"+cevreHesapla1);
+
+        System.out.println("Alan farkları Mutlak değer :"+Math.abs(alanHesapla-alanHesapla1));
+
+        System.out.println("Çevre farkları Mutlak değer :"+Math.abs(cevreHesapla-cevreHesapla1));
+
     }
-}
+
+    }
+
