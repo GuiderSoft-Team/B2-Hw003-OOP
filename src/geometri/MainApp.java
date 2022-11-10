e16f874073f6ccb3b698373f78ba93ffdf4a698c
Baristopcu08 <112826011+Baristopcu08@users.noreply.github.com>
2022-11-10 14:18:27 +0300

    Update

--- src/geometri/MainApp.java
+++ src/geometri/MainApp.java
@@ -5,7 +5,7 @@ public class MainApp {
         Kare kare = new Kare(7);
         Daire daire = new Daire(7);
 
-        System.out.printf("Karenin alan/çevre farkı : %5.2f\nDairenin alan/çevre farkı : %5.2f ",
+        System.out.printf("Kare/Daire alan farkı : %5.2f\nKare/Daire çevre farkı : %5.2f ",
                             Math.abs(kare.alanHesapla() - daire.alanHesapla()),
                             Math.abs(kare.cevreHesapla() - daire.cevreHesapla()));
 
