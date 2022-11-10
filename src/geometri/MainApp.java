f6917c023974203ba9c47b2cafbbb336584af0b9
GİZEM YAYLAGÜL <gizemyaylagul@gmail.com>
2022-11-10 13:25:43 +0300

    Gizem Yaylagül Ödev 3 tamamlandı.

--- src/geometri/MainApp.java
+++ src/geometri/MainApp.java
@@ -2,7 +2,20 @@ package geometri;
 
 public class MainApp {
     public static void main(String[] args) {
+        int kareninAlan=Kare.alanHesapla(7);
+        System.out.println("Karenin Alanı : "+kareninAlan);
+        int kareninCevre=Kare.cevreHesapla(7);
+        System.out.println("Karenin Çevresi :"+kareninCevre);
+        double kosegeninUzunlugu=Kare.kosegenUzunlugu(7);
+        System.out.println("Köşegenin Uzunluğu : "+Kare.kosegenUzunlugu(7));
         System.out.println();
-        //Lütfen readme.md dosyasına göre ödevi tamamlayınız.
+        double daireninAlan=Daire.alanHesapla(7);
+        System.out.println("Dairenin Alanı : "+daireninAlan);
+        double daireninCevre=Daire.cevreHesapla(7);
+        System.out.println("Dairenin Çevresi : "+daireninCevre);
+        System.out.println("Alanlarının Farkı : "+Math.abs(kareninAlan-daireninAlan));
+        System.out.println("Çevrelerinin Farkı :"+Math.abs(kareninCevre-daireninCevre));
+
+
     }
 }
