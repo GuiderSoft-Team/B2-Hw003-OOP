d08db84acf9ea87dfa59e42ac1ca15c342ad0695
Acer <erhanf5534@gmail.com>
2022-11-10 19:13:28 +0300

    ödev tamamlandı

--- src/geometri/MainApp.java
+++ src/geometri/MainApp.java
@@ -4,5 +4,29 @@ public class MainApp {
     public static void main(String[] args) {
         System.out.println();
         //Lütfen readme.md dosyasına göre ödevi tamamlayınız.
+
+        int kareninAlani=Kare.alanHesapla(7);
+        System.out.println("Karenin Alanı: "+kareninAlani);
+
+        int kareninCevresi=Kare.cevreHesapla(7);
+        System.out.println("Karenin Cevresi: "+kareninCevresi);
+
+        double kosegenUzunlugu=Kare.kosegenUzunlugu(7);
+        System.out.printf("Köşegenin Uzunluğu: %5.2f\n ",kosegenUzunlugu);
+        System.out.println();
+
+        double daireninAlani=Daire.alanHesapla(7);
+        System.out.printf("Dairenin Alanı: %5.2f\n",daireninAlani);
+
+        double daireninCevresi=Daire.cevreHesapla(7);
+        System.out.printf("Dairenin Cevresi: %5.2f\n ",daireninCevresi);
+        System.out.println();
+
+
+        System.out.printf("Alanlar Farkkı: %5.2f\n",Math.abs(kareninAlani-daireninAlani));
+        System.out.printf("Çevreler Farkkı: %5.2f\n",Math.abs(kareninCevresi-daireninCevresi));
+
+
     }
+
 }
