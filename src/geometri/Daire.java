e3a538b412d27a686280c702cca9034959e600f8
ondergulsen <ondergulsenn@gmail.com>
2022-11-10 18:47:43 +0300

    geometri ödevi tamamlandı

---
+++ src/geometri/Daire.java
@@ -0,0 +1,21 @@
+package geometri;
+public class Daire {
+
+
+
+    public int yaricap;
+
+    public Daire(int yaricap) {
+        this.yaricap = yaricap;
+    }
+    public double alanHesapla(){
+        return Math.round(Math.PI * yaricap * yaricap);
+
+    }
+    public double cevreHesapla() {
+        return Math.round(2 * Math.PI * yaricap);
+    }
+    public String toString() {
+        return "Daire yarıçapı=" + this.yaricap + " Alanı= " + alanHesapla() + " Çevresi= " + cevreHesapla();
+    }
+}
--- src/geometri/MainApp.java
+++ src/geometri/MainApp.java
@@ -2,7 +2,15 @@ package geometri;
 
 public class MainApp {
     public static void main(String[] args) {
+        Kare square = new Kare(9);
+        Daire circle = new Daire(5);
+
+        System.out.println(square);
+        System.out.println(circle);
+        double alanFarki = Math.abs(circle.alanHesapla() - square.alanHesapla());
+        double cevreFarki = Math.abs(circle.cevreHesapla() - square.cevreHesapla());
+
+        System.out.printf("Alanlar farkı: %5.2f ve Çevreler farkı: %5.2f", alanFarki, cevreFarki);
         System.out.println();
-        //Lütfen readme.md dosyasına göre ödevi tamamlayınız.
     }
 }
