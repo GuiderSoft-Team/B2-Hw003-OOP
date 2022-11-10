eb4e56cc1c5f855f6c35b7a630c0b8d244649839
Çağla <mariliith@hotmail.com>
2022-11-10 13:43:36 +0300

    Çağlasu_Büke_Homework3

---
+++ src/geometri/Daire.java
@@ -0,0 +1,11 @@
+package geometri;
+
+public class Daire {
+    public static double alanHesapla(int r){
+        return Math.PI*r*r;
+
+    }
+    public static double cevreHesapla(int r){
+        return 2*Math.PI*r;
+    }
+}
