fb47755ee9e4e6a1655bec6958aa49bdaca05910
nizamimtest <nizamimtest@gmail.com>
2022-11-10 19:43:42 +0400

    Nizami Muradov odevi yapti

---
+++ src/geometri/Cevre.java
@@ -0,0 +1,16 @@
+package geometri;
+
+public class Cevre {
+        int yaricap;
+
+        public static double alanHesapla(double yaricap) {
+            return Math.PI * yaricap * yaricap;
+        }
+
+        public static double cevreHesapla(double yaricap) {
+
+            return 2 * Math.PI * yaricap;
+        }
+
+    }
+
