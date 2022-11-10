b43eea80a3794551ac341fb7d13fe2dbc66b479d
suleymandurmaz <sdurmaz77@hotmail.com>
2022-11-10 17:09:14 +0300

    hw003 done by SULEYMAN

---
+++ src/geometri/Daire.java
@@ -0,0 +1,19 @@
+package geometri;
+
+public class Daire {
+    public double yaricap;
+
+    public  Daire(double yaricap) {
+        this.yaricap = yaricap;
+
+    }
+
+    public double alanHesapla(double yaricap){
+
+        return Math.PI*yaricap*yaricap;
+    }
+    public double cevreHesapla(double yaricap){
+
+        return 2*Math.PI*yaricap;
+    }
+}
