88f35a84fa8275b519c8e3a46a80e7a00e64fef5
user <onikaankaymaz@gmail.com>
2022-11-10 13:06:42 +0300

    Kaan Kaymaz Homework003 10.11.2022

--- src/geometri/MainApp.java
+++ src/geometri/MainApp.java
@@ -2,7 +2,19 @@ package geometri;
 
 public class MainApp {
     public static void main(String[] args) {
+        int kareAlan = Kare.alanHesapla(7);
+        System.out.println("Karenin alanı : "+kareAlan);
+        int kareCevre = Kare.cevreHesapla(7);
+        System.out.println("Karenin çevresi :"+kareCevre);
+        double kosegenUzunlugu = Kare.kosegenUzunlugu(7);
+        System.out.println("Karenin köşegen uzunluğu : "+kosegenUzunlugu);
         System.out.println();
-        //Lütfen readme.md dosyasına göre ödevi tamamlayınız.
+        double daireAlan = Daire.alanHesapla(7);
+        System.out.println("Dairenin alanı : "+daireAlan);
+        double daireCevre = Daire.cevreHesapla(7);
+        System.out.println("Dairenin çevresi : "+daireCevre);
+        System.out.println();
+        System.out.println("Alanların farkı : "+Math.abs(kareAlan-daireAlan));
+        System.out.println("Çevrelerin farkı : "+Math.abs(kareCevre-daireCevre));
     }
 }
