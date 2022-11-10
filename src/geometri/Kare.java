1756b97bad4e4f9c55a10e414ca62512705205c0
ademgencer <ademyasargencer@gmail.com>
2022-11-10 10:54:25 +0300

    Homework 3

---
+++ src/geometri/Kare.java
@@ -0,0 +1,28 @@
+package geometri;
+
+public class Kare {
+
+    int kenar;
+
+    public Kare(int kenar) {
+
+        this.kenar = kenar;
+    }
+
+    public int alanHesapla() {
+
+        return kenar * kenar;
+    }
+
+    public int cevreHesapla() {
+
+        return 4 * kenar;
+    }
+
+    public double kosegenUzunlugu() {
+
+        return kenar * Math.sqrt(2);
+    }
+
+
+}
--- src/geometri/MainApp.java
+++ src/geometri/MainApp.java
@@ -2,7 +2,16 @@ package geometri;
 
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println();
-        //Lütfen readme.md dosyasına göre ödevi tamamlayınız.
+
+        Kare square = new Kare(7);
+
+        Daire circle = new Daire(7);
+
+        System.out.println("Karenin alan ve çevre faarkı : "
+                + Math.abs(square.alanHesapla() - square.cevreHesapla()));
+
+        System.out.println("Dairenin alan ve çevre farkı : "
+                + Math.abs(circle.alanHesapla() - circle.cevreHesapla()));
+
     }
 }
