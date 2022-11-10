dcc7873773511f8c1f0b2d4a362fe76c21dee59b
Omer Nalci <oemer.nalci@gmail.com>
2022-11-10 16:56:47 +0100

    Hausaufgaben HW003 erledigt.

--- src/geometri/MainApp.java
+++ src/geometri/MainApp.java
@@ -2,7 +2,16 @@ package geometri;
 
 public class MainApp {
     public static void main(String[] args) {
+
+        Kare kare=new Kare(7);
+        Daire daire=new Daire(7);
+
+        double alanFarki= kare.alanHesapla()- daire.alanHesapla();
+        double cevreFarki= kare.cevreHesapla()- daire.cevreHesapla();
+
+        System.out.printf("Alanlarin mutlak farki = %3.2f cm^2\n",Math.abs(alanFarki));
         System.out.println();
-        //Lütfen readme.md dosyasına göre ödevi tamamlayınız.
+        System.out.printf("Cevrelerin mutlak farki = %3.2f cm",Math.abs(cevreFarki));
+
     }
 }
