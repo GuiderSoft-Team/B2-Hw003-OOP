8292391a8b0ed3bb656f5d115a018c489618f872
Aysun Ardıç <aysnardc0706@gmail.com>
2022-11-11 13:44:23 +0300

    Hw3bıttı

---
+++ src/geometri/Daire.java
@@ -0,0 +1,13 @@
+package geometri;
+
+public class Daire {
+
+    public static double alanHesapla(int yarıcap) {
+
+        return Math.PI*yarıcap*yarıcap;
+    }
+    public static double cevreHesapla(int yaricap){
+        return 2*Math.PI*yaricap;
+    }
+
+}
